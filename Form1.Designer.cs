5d63d1ed095d882b863d64e315c2d57f0ec29a16
Yasaswini7798 <86249768+Yasaswini7798@users.noreply.github.com>
2021-07-28 18:48:18 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Day15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.Black;
+            this.btn0.Location = new System.Drawing.Point(142, 101);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 71);
+            this.btn0.TabIndex = 0;
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(290, 101);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 71);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(433, 101);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 71);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(142, 227);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 71);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(290, 227);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 71);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(433, 227);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 71);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(142, 345);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 71);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(290, 345);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 71);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(433, 345);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 71);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.buttonsclicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day15
+{
+    public partial class Form1 : Form
+    {
+        public int moves;
+        public int[] a;
+        public int res;
+        public Form1()
+        {
+            InitializeComponent();
+            moves = 1;
+            a = new int[9];
+            for (int i = 0; i < 9; i++)
+                a[i] = 0;
+            res = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void buttonsclicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int ind;
+            string name = btn.Name;
+            if (moves < 10 && res == -1)
+            {
+                if (moves % 2 == 1)
+                {
+                    if (btn.BackColor != Color.Black)
+                    {
+                        MessageBox.Show("Click other button as its occupied");
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Yellow;
+                        ind = int.Parse(name.Substring(3));
+                        a[ind] = 1;
+                        moves++;
+                    }
+                }
+                else
+                {
+                    if (btn.BackColor != Color.Black)
+                    {
+                        MessageBox.Show("Click other button as its occupied");
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Red;
+                        ind = int.Parse(name.Substring(3));
+                        a[ind] = 2;
+                        moves++;
+                    }
+                }
+                if (moves > 5)
+                    res = checkwinner();
+                if (res != -1)
+                    MessageBox.Show("Congrats to player" + res);
+            }
+            if (moves == 10 && res == -1)
+                MessageBox.Show("Match draw");
+        }
+            public int checkwinner()
+            {
+                int[] players = new int[2] { 1, 2 };
+            int flag = -1;
+            foreach(int p in players)
+            {
+                if((a[0]==p&&a[1]==p&&a[2]==p)||
+                    (a[3] == p && a[4] == p && a[5] == p)||
+                        (a[6] == p && a[7] == p && a[8] == p)||
+                    (a[0] == p && a[3] == p && a[6] == p)||
+                    (a[1] == p && a[4] == p && a[7] == p)||
+                    (a[2] == p && a[5] == p && a[8] == p)||
+                    (a[0] == p && a[4] == p && a[8] == p)||
+                    (a[2] == p && a[4] == p && a[6] == p))
+                {
+                    flag = 0;
+                    return p;
+                }
+            }
+            return flag;
+            }
+        }
+    }
+
